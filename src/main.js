b873586034e50bef01e31fa03af75ac1759d32e1
abww-00 <gailalmazan@gmail.com>
2025-12-16 17:51:36 +0800

    commit-5

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 
 // PRIME VUE
 import PrimeVue from 'primevue/config';
-import 'primevue/resources/themes/aura-light-green/theme.css'
+import 'primevue/resources/themes/aura-dark-blue/theme.css'
 import 'primevue/resources/primevue.min.css'
 import 'primeicons/primeicons.css'
 
@@ -12,7 +12,24 @@ const app = createApp(App)
 app.use(PrimeVue)
 
 // PRIME VUE - COMPONENTS
-import Button from 'primevue/button'
-app.component('Button', Button)
+import IconField from 'primevue/iconfield';
+import InputIcon from 'primevue/inputicon';
+app.component('IconField', IconField);
+app.component('InputIcon', InputIcon);
+
+import Dropdown from 'primevue/dropdown';
+app.component('Dropdown', Dropdown);
+
+import Checkbox from 'primevue/checkbox';
+app.component('Checkbox', Checkbox);
+
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import ColumnGroup from 'primevue/columngroup';   // optional
+import Row from 'primevue/row';                   // optional
+app.component('DataTable', DataTable);
+app.component('Column', Column);
+app.component('ColumnGroup', ColumnGroup);
+app.component('Row', Row);
 
 app.mount('#app')
